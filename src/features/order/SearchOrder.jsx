2efc431290bc0160def3d4d81d2b1531b148bce1
mack14 <mugishamackson15@gmail.com>
2023-10-02 17:04:00 +0300

    fast pizza project

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+function SearchOrder() {
+  const [query, setQuery] = useState("");
+  const navigate = useNavigate();
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Search Order "
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        className="rounded-full bg-yellow-100 w-28 px-4 py-2 text-sm transition-all duration-300 placeholder:text-stone-400 sm:focus:w-72 sm:w-64 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-opacity-50"
+      />
+    </form>
+  );
+}
+
+export default SearchOrder;
